650eb8f8c675c1d5149ac52eb8429c7a854a648f
lubigsis <lucre.baluc@gmail.com>
2023-03-03 23:43:58 -0300

    Agrego funciones en JS y edito otros errores de tipeo

--- main.js
+++ main.js
@@ -2,18 +2,16 @@
 let inputs, clock, alarma, horas, minutos, segundos, repetidor;
 
 //-------------------------------------------------------------------------------------------------------------
-
 window.addEventListener('load', () => { /* Espero a que cargue el documento HTML */
     inputs = Array.from(document.getElementsByClassName('numero')); /* Busco los inputs */
     clock = document.querySelector('.reloj'); /* Busco el reloj p/poder luego cambiar los nros a medida q' pasa el tiempo*/
-    alarma = new Audio('alarm.mp3'); /* Genero la alarma */
+    alarma = new Audio('sound_alarm.mp3'); /* Genero la alarma */
 });
 
 /*----------------------------------------------------------Funciones------------------------------------- */
 //Función ppal.
 //---creo una función q' en base al llamado de otras funciones, haga q' el temporizador arranque
 //startTimer() primero lee los inputs, luego actualiza la vista y la pestaña del navegador, dsp arranca el temporizador.
-
 function startTimer() { 
     parseTime(); /* Busca y transforma los valores del input a numeros */
     setTimer();  /* Setea el temporizador visualmente */
@@ -33,7 +31,7 @@ function parseTime() {
 
 //---------------------------------------------------------
 
-/* Funcion para cambiar el temporizador en la pantalla y en la pestaña */
+/* Función para cambiar el temporizador en la pantalla y en la pestaña */
 //actualiza pantalla c/los valores de las variables (lo q' ingrese el user)
 function setTimer() {
     /* Cambio la hora en pantalla */
@@ -54,8 +52,41 @@ function setTimer() {
 //-------------------------------------------------------
 //función q' arranca el contador
 //en la variable reoetidor se guarda lo que ejecute setInterval Falta declarar la función 'runner' q' será llamada c/1"
+//inicia y frena el timer
 function countdown() {
     repetidor = setInterval(runner,1000);
 }
 
 /*--------------------------------------------------------- */
+/*----- Funcion que cuenta cuanto queda, y es llamada por la función countdown() cada 1" */
+function runner() {
+    /* Si tengo más de 0 segundos, restá segundos */
+    /* Si tengo 0 segundos pero tengo más de 0 minutos, poné segundos en 59 y restale 1 a minutos */
+    /* Si tengo 0 segundos, 0 minutos pero tengo más de 0 horas, poné segundos en 59, minutos en 59 y restale 1 a horas */
+    /* Sino arranca la alarma */
+    
+    if (segundos > 0) {
+        segundos--;
+    } else {
+        if (minutos > 0) {
+            segundos = 59;
+            minutos--;
+        } else {
+            if (horas > 0 ) {
+                segundos = 59;
+                minutos = 59;
+                horas--;
+            } else {
+                alarma.play();
+            }
+        }
+    }
+    
+    setTimer();//la llama para actualizar valores.
+}
+
+/* Funcion para detener el timer */
+function stopTimer(){
+    clearInterval(repetidor);
+    location.reload();
+}
